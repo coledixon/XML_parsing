37d279410f2c2fa70e0518c208ef85e0d79f1885
colefromportland <colefromportland@gmail.com>
2020-03-25 11:57:11 -0600

    build XML file structure

--- XML_parseForm/XML_parseForm/Program.cs
+++ XML_parseForm/XML_parseForm/Program.cs
@@ -16,7 +16,7 @@ namespace XML_parseForm
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new XMLparser());
         }
     }
 }
