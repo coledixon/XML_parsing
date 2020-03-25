93289cf2f7aa21b6278fa876981fb3e98732a47e
colefromportland <colefromportland@gmail.com>
2020-03-25 11:56:50 -0600

    rename form /  add elements

---
+++ XML_parseForm/XML_parseForm/XMLparser.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace XML_parseForm
+{
+    partial class XMLparser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.lvOutput = new System.Windows.Forms.ListView();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(12, 24);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(376, 26);
+            this.txtSearch.TabIndex = 0;
+            // 
+            // lvOutput
+            // 
+            this.lvOutput.HideSelection = false;
+            this.lvOutput.Location = new System.Drawing.Point(12, 70);
+            this.lvOutput.Name = "lvOutput";
+            this.lvOutput.Size = new System.Drawing.Size(599, 299);
+            this.lvOutput.TabIndex = 1;
+            this.lvOutput.UseCompatibleStateImageBehavior = false;
+            this.lvOutput.View = System.Windows.Forms.View.List;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(409, 18);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(202, 38);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "SEARCH";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // XMLparser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 383);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.lvOutput);
+            this.Controls.Add(this.txtSearch);
+            this.Name = "XMLparser";
+            this.Text = "XML Parser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.ListView lvOutput;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
+
---
+++ XML_parseForm/XML_parseForm/XMLparser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace XML_parseForm
+{
+    public partial class XMLparser : Form
+    {
+        public XMLparser()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            // validate input has value and is more than 3 chars
+            if (!string.IsNullOrEmpty(txtSearch.Text) && txtSearch.Text.Length >= 3)
+            {
+                // instantiate XML doc
+                XmlDocument doc = new XmlDocument();
+            }
+            else {
+                MessageBox.Show("INVALID SEARCH INPUT");
+                txtSearch.Text = string.Empty; // reset textbox
+                txtSearch.Focus(); // refocus on control
+            }
+        }
+    }
+}
