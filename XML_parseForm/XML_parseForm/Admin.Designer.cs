895b3aa6a3be2b22a0cd0fb38df4a4093e8c7ca6
colefromportland <colefromportland@gmail.com>
2020-03-25 17:14:34 -0600

    build && design Admin form

---
+++ XML_parseForm/XML_parseForm/Admin.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace XML_parseForm
+{
+    partial class Admin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtYear = new System.Windows.Forms.TextBox();
+            this.txtDirector = new System.Windows.Forms.TextBox();
+            this.txtCast = new System.Windows.Forms.TextBox();
+            this.txtImdb = new System.Windows.Forms.TextBox();
+            this.txtTomato = new System.Windows.Forms.TextBox();
+            this.txtCritic = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblYear = new System.Windows.Forms.Label();
+            this.lblDirector = new System.Windows.Forms.Label();
+            this.lblCast = new System.Windows.Forms.Label();
+            this.lblImdb = new System.Windows.Forms.Label();
+            this.lblTomatoes = new System.Windows.Forms.Label();
+            this.lblCritic = new System.Windows.Forms.Label();
+            this.lblImdb2 = new System.Windows.Forms.Label();
+            this.lblTomatoes2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(69, 22);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(456, 26);
+            this.txtTitle.TabIndex = 0;
+            // 
+            // txtYear
+            // 
+            this.txtYear.Location = new System.Drawing.Point(153, 54);
+            this.txtYear.Name = "txtYear";
+            this.txtYear.Size = new System.Drawing.Size(68, 26);
+            this.txtYear.TabIndex = 1;
+            // 
+            // txtDirector
+            // 
+            this.txtDirector.Location = new System.Drawing.Point(111, 86);
+            this.txtDirector.Name = "txtDirector";
+            this.txtDirector.Size = new System.Drawing.Size(414, 26);
+            this.txtDirector.TabIndex = 2;
+            // 
+            // txtCast
+            // 
+            this.txtCast.Location = new System.Drawing.Point(69, 118);
+            this.txtCast.Name = "txtCast";
+            this.txtCast.Size = new System.Drawing.Size(456, 26);
+            this.txtCast.TabIndex = 3;
+            // 
+            // txtImdb
+            // 
+            this.txtImdb.Location = new System.Drawing.Point(69, 150);
+            this.txtImdb.Name = "txtImdb";
+            this.txtImdb.Size = new System.Drawing.Size(36, 26);
+            this.txtImdb.TabIndex = 4;
+            // 
+            // txtTomato
+            // 
+            this.txtTomato.Location = new System.Drawing.Point(186, 182);
+            this.txtTomato.Name = "txtTomato";
+            this.txtTomato.Size = new System.Drawing.Size(35, 26);
+            this.txtTomato.TabIndex = 5;
+            // 
+            // txtCritic
+            // 
+            this.txtCritic.Location = new System.Drawing.Point(124, 214);
+            this.txtCritic.Name = "txtCritic";
+            this.txtCritic.Size = new System.Drawing.Size(53, 26);
+            this.txtCritic.TabIndex = 6;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(12, 255);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(165, 50);
+            this.btnAdd.TabIndex = 7;
+            this.btnAdd.Text = "ADD";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(186, 255);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(165, 50);
+            this.btnClear.TabIndex = 8;
+            this.btnClear.Text = "CLEAR";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(360, 255);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(165, 50);
+            this.btnClose.TabIndex = 9;
+            this.btnClose.Text = "CLOSE";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(12, 22);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(52, 20);
+            this.lblTitle.TabIndex = 10;
+            this.lblTitle.Text = "TITLE";
+            // 
+            // lblYear
+            // 
+            this.lblYear.AutoSize = true;
+            this.lblYear.Location = new System.Drawing.Point(12, 54);
+            this.lblYear.Name = "lblYear";
+            this.lblYear.Size = new System.Drawing.Size(134, 20);
+            this.lblYear.TabIndex = 11;
+            this.lblYear.Text = "RELEASE YEAR";
+            // 
+            // lblDirector
+            // 
+            this.lblDirector.AutoSize = true;
+            this.lblDirector.Location = new System.Drawing.Point(12, 86);
+            this.lblDirector.Name = "lblDirector";
+            this.lblDirector.Size = new System.Drawing.Size(93, 20);
+            this.lblDirector.TabIndex = 12;
+            this.lblDirector.Text = "DIRECTOR";
+            // 
+            // lblCast
+            // 
+            this.lblCast.AutoSize = true;
+            this.lblCast.Location = new System.Drawing.Point(12, 118);
+            this.lblCast.Name = "lblCast";
+            this.lblCast.Size = new System.Drawing.Size(51, 20);
+            this.lblCast.TabIndex = 13;
+            this.lblCast.Text = "CAST";
+            // 
+            // lblImdb
+            // 
+            this.lblImdb.AutoSize = true;
+            this.lblImdb.Location = new System.Drawing.Point(12, 150);
+            this.lblImdb.Name = "lblImdb";
+            this.lblImdb.Size = new System.Drawing.Size(50, 20);
+            this.lblImdb.TabIndex = 14;
+            this.lblImdb.Text = "IMDB";
+            // 
+            // lblTomatoes
+            // 
+            this.lblTomatoes.AutoSize = true;
+            this.lblTomatoes.Location = new System.Drawing.Point(12, 182);
+            this.lblTomatoes.Name = "lblTomatoes";
+            this.lblTomatoes.Size = new System.Drawing.Size(165, 20);
+            this.lblTomatoes.TabIndex = 15;
+            this.lblTomatoes.Text = "ROTTEN TOMATOES";
+            // 
+            // lblCritic
+            // 
+            this.lblCritic.AutoSize = true;
+            this.lblCritic.Location = new System.Drawing.Point(12, 214);
+            this.lblCritic.Name = "lblCritic";
+            this.lblCritic.Size = new System.Drawing.Size(106, 20);
+            this.lblCritic.TabIndex = 16;
+            this.lblCritic.Text = "METACRITIC";
+            // 
+            // lblImdb2
+            // 
+            this.lblImdb2.AutoSize = true;
+            this.lblImdb2.Location = new System.Drawing.Point(107, 153);
+            this.lblImdb2.Name = "lblImdb2";
+            this.lblImdb2.Size = new System.Drawing.Size(31, 20);
+            this.lblImdb2.TabIndex = 17;
+            this.lblImdb2.Text = "/10";
+            // 
+            // lblTomatoes2
+            // 
+            this.lblTomatoes2.AutoSize = true;
+            this.lblTomatoes2.Location = new System.Drawing.Point(227, 185);
+            this.lblTomatoes2.Name = "lblTomatoes2";
+            this.lblTomatoes2.Size = new System.Drawing.Size(23, 20);
+            this.lblTomatoes2.TabIndex = 18;
+            this.lblTomatoes2.Text = "%";
+            // 
+            // Admin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(540, 331);
+            this.Controls.Add(this.lblTomatoes2);
+            this.Controls.Add(this.lblImdb2);
+            this.Controls.Add(this.lblCritic);
+            this.Controls.Add(this.lblTomatoes);
+            this.Controls.Add(this.lblImdb);
+            this.Controls.Add(this.lblCast);
+            this.Controls.Add(this.lblDirector);
+            this.Controls.Add(this.lblYear);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtCritic);
+            this.Controls.Add(this.txtTomato);
+            this.Controls.Add(this.txtImdb);
+            this.Controls.Add(this.txtCast);
+            this.Controls.Add(this.txtDirector);
+            this.Controls.Add(this.txtYear);
+            this.Controls.Add(this.txtTitle);
+            this.Name = "Admin";
+            this.Text = "Admin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtYear;
+        private System.Windows.Forms.TextBox txtDirector;
+        private System.Windows.Forms.TextBox txtCast;
+        private System.Windows.Forms.TextBox txtImdb;
+        private System.Windows.Forms.TextBox txtTomato;
+        private System.Windows.Forms.TextBox txtCritic;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblYear;
+        private System.Windows.Forms.Label lblDirector;
+        private System.Windows.Forms.Label lblCast;
+        private System.Windows.Forms.Label lblImdb;
+        private System.Windows.Forms.Label lblTomatoes;
+        private System.Windows.Forms.Label lblCritic;
+        private System.Windows.Forms.Label lblImdb2;
+        private System.Windows.Forms.Label lblTomatoes2;
+    }
+}
---
+++ XML_parseForm/XML_parseForm/Admin.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace XML_parseForm
+{
+    public partial class Admin : Form
+    {
+        public Admin()
+        {
+            InitializeComponent();
+        }
+
+        #region buttonEvents
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            // DO ADD TO XML
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            // CLEAR ALL FORM FIELDS
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            // CLOSE / DISPOSE FORM
+        }
+        #endregion buttonEvents
+    }
+}
